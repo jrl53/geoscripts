e3d6523e3be90861e4ea196ba9addfb90c01f9b9
jrl53 <josericardoleonlee@gmail.com>
2018-04-01 21:43:19 +0530

    Update parseExcel2.js

--- parseExcel2.js
+++ parseExcel2.js
@@ -21,7 +21,7 @@ const cs = new pgp.helpers.ColumnSet([
     'vehicle','driver', 'thegroup', 'thedate', 'event', 'latitude', 'longitude', 'address'
 ], {table: 'azugalog'});
 
-var data = [];
+
 var files = [];
 
 var folder = 'files/'
@@ -37,7 +37,7 @@ console.log("Total of " + files.length + "files to process...");
 async function processFiles(){
     
     for(const item of files){
-
+        
         console.log("Starting with " + item);
         var ws = xlsx.parse(folder+item);
 
@@ -85,10 +85,76 @@ async function processFiles(){
     console.log("looks like we are outside");
 }
 
+/*
 processFiles().catch(function(err){
     console.log("there was a big error");
     console.log(err);
 })
+*/
+
+function getNextData(t, pageIndex) {
+    console.log("pageIndex =" + pageIndex);
+    let data = null;
+    if (pageIndex < files.length) {
+        data = [];
+        console.log("Starting with " + files[pageIndex]);
+        var ws = xlsx.parse(folder+files[pageIndex]);
+
+        var query = "";
+
+        if (ws.length < 1){
+            console.log("Error loading excel file");
+            return;
+        } 
+
+        console.log("sheet length = " + ws[0].data.length);
+
+
+        for(var i=6; i<ws[0].data.length; i++){
+            var dl = ws[0].data[i];
+
+            if(dl.length == 0) {
+                break;
+            }
+
+            var toPush = {
+                vehicle: dl[0],
+                driver: dl[1],
+                thegroup: dl[2],
+                thedate: dl[3],
+                event: dl[4],
+                latitude: dl[5],
+                longitude: dl[6],
+                address: dl[7]
+            }
+
+            data.push(toPush);
+        //        console.log(line);
+        }
+    }
+    return Promise.resolve(data);
+}
+
+
+db.tx('massive-insert', t => {
+    return t.sequence(index => {
+        return getNextData(t, index)
+            .then(data => {
+                if (data) {
+                    const insert = pgp.helpers.insert(data, cs);
+                    return t.none(insert);
+                }
+            });
+    });
+})
+    .then(data => {
+        // COMMIT has been executed
+        console.log('Total batches:', data.total, ', Duration:', data.duration);
+    })
+    .catch(error => {
+        // ROLLBACK has been executed
+        console.log(error);
+    });
 
 /*
 console.log("sending query..." + query.length);
